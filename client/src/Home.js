441e2bbbb5d2da532b383bef4b83be63f8edd3b6
Van Pham <thyvan1412@gmail.com>
2023-03-14 17:10:01 -0700

    added search function

--- client/src/Home.js
+++ client/src/Home.js
@@ -5,10 +5,45 @@ import Container from 'react-bootstrap/esm/Container';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import ControlledCarousel from './ControlledCarousel';
+import { useState } from 'react';
 
 function Home() {
-    function Search() {
+    const [searchKey, setSearchKey] = useState('');
+    const [searchType, setSearchType] = useState('artist');
+    const [artistSearch, setArtistSearch] = useState(false);
+    const [albumSearch, setAlbumSearch] = useState(false);
+    const [trackSearch, setTrackSearch] = useState(false);
 
+    function Search() {
+        console.log(searchKey)
+        console.log(searchType)
+        if (searchType === 'artist') {
+            setAlbumSearch(false)
+            setTrackSearch(false)
+            setArtistSearch(true)
+        }
+        else if (searchType === 'album') {
+            setTrackSearch(false)
+            setArtistSearch(false)
+            setAlbumSearch(true)
+        }
+        if (searchType === 'track') {
+            setAlbumSearch(false)
+            setArtistSearch(false)
+            setTrackSearch(true)
+        }
+        // fetch data
+        fetch(`http://localhost:8888/search/${searchType}/${encodeURIComponent(searchKey)}`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error('could not fetch data for that resource')
+            }
+            return res.json();
+        }).then(data => {
+            console.log(data)
+        }). catch(err => {
+            console.log(err.message)
+        })
     }
 
     return (
@@ -20,11 +55,11 @@ function Home() {
                     <Form className="mt-3">
                         <Row>
                             <Form.Group as={Col} className="mb-3" controlId="formBasicEmail">
-                                <Form.Control type="text"/>
+                                <Form.Control type="text" required value={searchKey} onChange={(e) => setSearchKey(e.target.value)}/>
                             </Form.Group>
 
                             <Form.Group as={Col} className="mb-3" controlId="formBasicEmail">
-                                <Form.Select className="w-50" aria-label="Default select example">
+                                <Form.Select value={searchType} onChange={(e) => setSearchType(e.target.value)} className="w-50" aria-label="Default select example">
                                     <option value="artist">artist</option>
                                     <option value="album">album</option>
                                     <option value="track">track</option>
@@ -35,8 +70,11 @@ function Home() {
                             Search
                         </Button>
                     </Form>
-
-                    <ControlledCarousel />
+                   
+                    {artistSearch && <ControlledCarousel />}
+                    {albumSearch && <div>hi</div>}
+                    {trackSearch && <div>ba</div>}  
+                
                 </Col>
 
             </Row>
